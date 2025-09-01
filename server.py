59ca1b5477046449bd367660a6663b27b10e03ca
KevinB <kevin@thinkwave.com>
2025-09-01 09:17:03 -0700

    made server.py file to launch local server

---
+++ server.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
+
+
+# to open local server from command line/terminal- use python3 server.py
